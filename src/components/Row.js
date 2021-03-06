17a376f34390c9252631c84d20f7794ddf0f9af5
thusharathiwanka <thusharathiwanka123@gmail.com>
2021-03-06 12:59:34 +0530

    CREATE : movie posters recieved

--- src/components/Row.js
+++ src/components/Row.js
@@ -2,12 +2,14 @@ import { Fragment, useState, useEffect } from "react";
 import axios from "../config/axios";
 
 const Row = ({ title, fetchUrl }) => {
+  const baseURLImg = "https://image.tmdb.org/t/p/original/";
   const [movies, setMovies] = useState([]);
 
   useEffect(() => {
     async function fetchMovies() {
       const request = await axios.get(fetchUrl);
       setMovies(request.data.results);
+      return request;
     }
     fetchMovies();
   }, [fetchUrl]);
@@ -15,7 +17,11 @@ const Row = ({ title, fetchUrl }) => {
   return (
     <Fragment>
       <h2>{title}</h2>
-      <div className="posters"></div>
+      <div className="posters">
+        {movies.map((movie) => (
+          <img src={`${baseURLImg}${movie.poster_path}`} alt={movie.name} />
+        ))}
+      </div>
     </Fragment>
   );
 };
